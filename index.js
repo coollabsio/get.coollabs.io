33c17e959f90123e7e4392a54a9a25cf911efd62
Andras Bacsai <andras.bacsai@gmail.com>
2024-01-10 10:43:59 +0100

    remove unnecessary things

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ if (!process.env.NOCODB_TOKEN) {
   throw new Error("NOCODB_TOKEN is not defined");
 }
 fastify.register(require("@fastify/cors"));
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "static"),
-});
+// fastify.register(require("@fastify/static"), {
+//   root: path.join(__dirname, "static"),
+// });
 
 fastify.get("/", function (req, reply) {
-  return reply.sendFile("index.html");
+  // return reply.sendFile("index.html");
+  return reply.redirect("https://coolify.io");
 });
 // fastify.get("/coolify/service-templates.yaml", function (req, reply) {
 //   return reply.redirect("https://coolcdn.b-cdn.net/coolify/service-templates.yaml");
@@ -21,6 +22,29 @@ fastify.get("/", function (req, reply) {
 // fastify.get("/coolify/service-tags.json", function (req, reply) {
 //   return reply.redirect("https://coolcdn.b-cdn.net/coolify/service-tags.json");
 // });
+
+fastify.get("/instances", async function (req, reply) {
+  if (req.headers["cool-api-key"] !== process.env.API_KEY) {
+    return reply.redirect("https://coolify.io");
+  }
+  const baseUrl = process.env.NOCODB_URL;
+  const nocodbUrl = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/InstanceCounter"
+  const instances = await fetch(nocodbUrl + "/count", {
+    headers: {
+      'xc-token': process.env.NOCODB_TOKEN
+    }
+  });
+  const nocodbUrlv4 = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/v4InstanceCounter"
+  const instancesv4 = await fetch(nocodbUrlv4 + "/count", {
+    headers: {
+      'xc-token': process.env.NOCODB_TOKEN
+    }
+  });
+  const json = await instances.json();
+  const jsonv4 = await instancesv4.json();
+
+  return { count: json.count + jsonv4.count };
+});
 fastify.get("/coolify/v4/alive", async function (req, reply) {
   const appId = req.query.appId;
   const version = req.query.version || "0.0.0";
@@ -66,80 +90,59 @@ fastify.get("/coolify/v4/alive", async function (req, reply) {
   return 'OK';
 
 });
-fastify.get("/versions.json", async function (req, reply) {
-  const appId = req.query.appId;
-  const baseUrl = process.env.NOCODB_URL;
-  const nocodbUrl = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/InstanceCounter"
-  const found = await fetch(nocodbUrl + "/find-one?where=where%28Uuid%2Ceq%2C" + appId + "%29", {
-    headers: {
-      'xc-token': process.env.NOCODB_TOKEN
-    }
-  })
-  const json = await found.json();
-  if (json && json?.Id) {
-    const payload = JSON.stringify({
-      LastSeen: new Date().getTime(),
-    });
-    fetch(nocodbUrl + '/' + json.Id, {
-      method: 'PATCH',
-      headers: {
-        'content-type': 'application/json',
-        'xc-token': process.env.NOCODB_TOKEN
-      },
-      body: payload
-    });
-  } else {
-    const payload = JSON.stringify({
-      Uuid: appId,
-      LastSeen: new Date().getTime(),
-    });
-    fetch(nocodbUrl, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'xc-token': process.env.NOCODB_TOKEN
-      },
-      body: payload
-    });
-  }
-  return reply.sendFile("versions.json");
-});
-fastify.get("/coolify/v4/instances", async function (req, reply) {
-  if (req.headers["cool-api-key"] !== process.env.API_KEY) {
-    return reply.redirect("https://coollabs.io");
-  }
-  const baseUrl = process.env.NOCODB_URL;
-  const nocodbUrl = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/v4InstanceCounter"
-  const instances = await fetch(nocodbUrl + "/count", {
-    headers: {
-      'xc-token': process.env.NOCODB_TOKEN
-    }
-  });
-  const json = await instances.json();
-  return { count: json.count };
-});
-fastify.get("/instances", async function (req, reply) {
-  if (req.headers["cool-api-key"] !== process.env.API_KEY) {
-    return reply.redirect("https://coollabs.io");
-  }
-  const baseUrl = process.env.NOCODB_URL;
-  const nocodbUrl = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/InstanceCounter"
-  const instances = await fetch(nocodbUrl + "/count", {
-    headers: {
-      'xc-token': process.env.NOCODB_TOKEN
-    }
-  });
-  const nocodbUrlv4 = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/v4InstanceCounter"
-  const instancesv4 = await fetch(nocodbUrlv4 + "/count", {
-    headers: {
-      'xc-token': process.env.NOCODB_TOKEN
-    }
-  });
-  const json = await instances.json();
-  const jsonv4 = await instancesv4.json();
+// fastify.get("/versions.json", async function (req, reply) {
+//   const appId = req.query.appId;
+//   const baseUrl = process.env.NOCODB_URL;
+//   const nocodbUrl = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/InstanceCounter"
+//   const found = await fetch(nocodbUrl + "/find-one?where=where%28Uuid%2Ceq%2C" + appId + "%29", {
+//     headers: {
+//       'xc-token': process.env.NOCODB_TOKEN
+//     }
+//   })
+//   const json = await found.json();
+//   if (json && json?.Id) {
+//     const payload = JSON.stringify({
+//       LastSeen: new Date().getTime(),
+//     });
+//     fetch(nocodbUrl + '/' + json.Id, {
+//       method: 'PATCH',
+//       headers: {
+//         'content-type': 'application/json',
+//         'xc-token': process.env.NOCODB_TOKEN
+//       },
+//       body: payload
+//     });
+//   } else {
+//     const payload = JSON.stringify({
+//       Uuid: appId,
+//       LastSeen: new Date().getTime(),
+//     });
+//     fetch(nocodbUrl, {
+//       method: 'POST',
+//       headers: {
+//         'content-type': 'application/json',
+//         'xc-token': process.env.NOCODB_TOKEN
+//       },
+//       body: payload
+//     });
+//   }
+//   return reply.sendFile("versions.json");
+// });
+// fastify.get("/coolify/v4/instances", async function (req, reply) {
+//   if (req.headers["cool-api-key"] !== process.env.API_KEY) {
+//     return reply.redirect("https://coolify.io");
+//   }
+//   const baseUrl = process.env.NOCODB_URL;
+//   const nocodbUrl = baseUrl + "/api/v1/db/data/noco/p8ovlkfbtnecctq/v4InstanceCounter"
+//   const instances = await fetch(nocodbUrl + "/count", {
+//     headers: {
+//       'xc-token': process.env.NOCODB_TOKEN
+//     }
+//   });
+//   const json = await instances.json();
+//   return { count: json.count };
+// });
 
-  return { count: json.count + jsonv4.count };
-});
 // fastify.get("/instances/seen", async function (req, reply) {
 //   if (req.headers["cool-api-key"] !== process.env.API_KEY) {
 //     return reply.redirect("https://coollabs.io");
